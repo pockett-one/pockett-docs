fe3b918b6c726f6296b364a8163fe79ce06beb93
Deepak Shettigar <deepaksshettigar@gmail.com>
2026-01-07 23:24:53 +0530

    feat: Design on global header & footer

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -614,6 +614,39 @@ export default function ConsultingLandingPage() {
           </FadeIn>
 
         </div>
+
+        {/* --- HERO SAND DUNES (Daytime Mode) --- */}
+        <div className="absolute inset-x-0 bottom-0 h-full w-full overflow-hidden pointer-events-none select-none z-0">
+          {/* Back Layer - Broad & Tall */}
+          <svg
+            className="absolute bottom-0 left-0 w-full h-full text-purple-50 fill-current transform scale-105"
+            preserveAspectRatio="none"
+            viewBox="0 0 1440 320"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0,192L60,197.3C120,203,240,213,360,208C480,203,600,181,720,176C840,171,960,181,1080,181.3C1200,181,1320,171,1380,165.3L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z" />
+          </svg>
+
+          {/* Middle Layer - distinct offsets */}
+          <svg
+            className="absolute bottom-0 left-0 w-full h-[60%] text-purple-100/60 fill-current"
+            preserveAspectRatio="none"
+            viewBox="0 0 1440 320"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0,128L80,138.7C160,149,320,171,480,165.3C640,160,800,128,960,122.7C1120,117,1280,139,1360,149.3L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+          </svg>
+
+          {/* Front Layer - Vibrant Accent */}
+          <svg
+            className="absolute -bottom-2 left-0 w-full h-[30%] text-purple-200/50 fill-current"
+            preserveAspectRatio="none"
+            viewBox="0 0 1440 320"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0,224L48,213.3C96,203,192,181,288,181.3C384,181,480,203,576,224C672,245,768,267,864,250.7C960,235,1056,181,1152,165.3C1248,149,1344,171,1392,181.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+          </svg>
+        </div>
       </section>
 
       {/* --- ARCHITECTURE & TRUST STRIP (Light/Motion Style) --- */}
